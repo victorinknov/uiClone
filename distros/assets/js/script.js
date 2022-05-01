93f13548741404c8737eb9b6f24153ab5e54e63e
Victor Inknov <victorhonly@gmail.com>
2022-05-01 16:34:11 -0300

    updating

--- distros/assets/js/script.js
+++ distros/assets/js/script.js
@@ -1,32 +1,51 @@
 // modals
-let modalAbout = document.getElementById('containerAbout'), 
+let modalAbout = document.getElementById('containerAbout'),
     modalDocs = document.getElementById('containerDocs')
 
 // buttons to open modals
 let btnAbout = document.getElementById('about'),
     btnDocs = document.getElementById('docs')
-    
+
 // buttons to close modals
 let btnCloseAbout = document.getElementById('closeAbout'),
     btnCloseDocs = document.getElementById('closeDocs')
 
+let main = document.querySelector('main'),
+    header = document.querySelector('header')
+
+console.error(main, header)
+
+
 // events to about modal
-btnAbout.addEventListener('click', ()=>{
+btnAbout.addEventListener('click', () => {
     modalAbout.classList.toggle('hidden')
+    main.classList.add('blur')
+    header.classList.add('blur')
 })
 
-btnCloseAbout.addEventListener('click', ()=>{
+btnCloseAbout.addEventListener('click', () => {
     modalAbout.classList.toggle('hidden')
+    main.classList.remove('blur')
+    header.classList.remove('blur')
+
 })
 
 // events to docs modal
-btnDocs.addEventListener('click', ()=>{
+btnDocs.addEventListener('click', () => {
     modalDocs.classList.toggle('hidden')
+    main.classList.add('blur')
+    header.classList.add('blur')
 })
 
-btnCloseDocs.addEventListener('click', ()=>{
+btnCloseDocs.addEventListener('click', () => {
     modalDocs.classList.toggle('hidden')
+    main.classList.remove('blur')
+    header.classList.remove('blur')
 })
 
 
-
+// prevent user drag images
+let allImages = document.querySelectorAll('img')
+allImages.forEach(element => {
+    element.setAttribute('draggable', 'false')
+})
