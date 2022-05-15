a6cd04c47846a9ecb558f37a59da97c40a70d5e2
Victor Inknov <victorhonly@gmail.com>
2022-05-14 22:31:27 -0300

    adding drag to error

--- distros/windows10/assets/js/login.js
+++ distros/windows10/assets/js/login.js
@@ -153,6 +153,45 @@ closesModal.forEach(element => {
 function errorMessage (event, message) {
     let textError = document.getElementById('textError')
     textError.innerText = message
+    
     let modalErrorUserNotFound = document.getElementById('modalErrorUserNotFound')
     event == 'open' ? modalErrorUserNotFound.style.display = 'flex' : event == 'close' ? modalErrorUserNotFound.style.display = 'none' : null
 }
+
+
+interact('.modalErrorUserNotFound')
+  .draggable({
+    // enable inertial throwing
+    inertia: false,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    // enable autoScroll
+    autoScroll: false,
+
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener,
+    }
+  })
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
